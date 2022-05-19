88fb6a284d3d80004b70b228317287bd2e379314
Feda Curic <feda.curic@gmail.com>
2022-05-19 09:31:46 +0200

    Turn on jupytext

---
+++ hdf5.py
@@ -0,0 +1,73 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Testing writing to $N$ `hdf5` files in parallel where each contains $p$ numbers
+
+# %%
+import numpy as np
+rng = np.random.default_rng(12345)
+import pandas as pd
+
+from p_tqdm import p_map
+
+# %%
+# Number of parameters
+p = 100000
+# Number of ensembles
+N = 100
+
+
+# %%
+def write_to_many(N):
+    df = pd.DataFrame(rng.normal(size=p), columns=[f"{N}"])
+    df.to_hdf(f"Ensemble_{N}.h5", key="df", mode="w")
+    return True
+
+
+# %% [markdown]
+# ## Write to files
+
+# %%
+# %%time
+res = p_map(write_to_many, np.arange(0, N))
+print(f"Writing to {N} hdf5 files where each contains {p} numbers takes:")
+
+# %% [markdown]
+# ## Read from files
+
+# %%
+# %%time
+df = pd.DataFrame(index=np.arange(0, p))
+for i in range(N):
+    df = df.join(pd.read_hdf(f"Ensemble_{i}.h5"))
+    
+print(f"Combining {N} hdf5 files containing {p} numbers each into one data frame takes:")
+
+# %%
+df.shape
+
+# %%
+assert df.shape == (p, N)
+
+# %% [markdown]
+# ## Clean up
+
+# %%
+from pathlib import Path
+for p in Path(".").glob("Ensemble_*.h5"):
+    p.unlink()
+
+# %%
